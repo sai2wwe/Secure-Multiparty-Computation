e51c82c915db5de687a59bb54d2bcda5d29a59f3
RaghavnedraRQ <sai783191@gmail.com>
2024-11-20 14:18:24 +0530

    add constants.py

--- mpc/protocols/ShamirPrep.py
+++ mpc/protocols/ShamirPrep.py
@@ -1,10 +1,11 @@
 from secrets import randbelow
+from config.constants import DEFAULT_PRIME, DEFAULT_THRESHOLD_DEGREE
 
-def generate_polynomial(secret, threshold_degree, prime):
+def generate_polynomial(secret, threshold_degree = DEFAULT_THRESHOLD_DEGREE, prime=DEFAULT_PRIME):
     return [secret] + [randbelow(prime) for _ in range(threshold_degree) ]
 
 
-def evaulate_polynomial(polynomial, x, prime):
+def evaulate_polynomial(polynomial, x, prime = DEFAULT_PRIME):
     
     total = 0
     for i, coef in enumerate(polynomial):
--- mpc/protocols/ShamirSharing.py
+++ mpc/protocols/ShamirSharing.py
@@ -1,11 +1,21 @@
 from mpc.protocols.ShamirPrep import generate_polynomial, evaulate_polynomial
+from config.constants import DEFAULT_PRIME, DEFAULT_THRESHOLD_DEGREE
 
-def generate_shares(secret, num_shares, threshold_degree, prime):
+def generate_shares(secret, num_shares, threshold_degree = DEFAULT_THRESHOLD_DEGREE, prime = DEFAULT_PRIME):
+    """
+    This funcion returns a list of shares of the secret. Each share is a tuple (x, y) 
+    where x is the share number and y is the share value.
+    @param secret: The secret to be shared
+    @param num_shares: The number of shares to generate
+    @param threshold_degree: The minimum number of shares required to reconstruct the secret
+    @param prime: A prime number greater than the secret
+    @return: A list of shares
+    """
     polynomial = generate_polynomial(secret, threshold_degree, prime)
     return [(i, evaulate_polynomial(polynomial, i, prime)) for i in range(1, num_shares + 1)]
 
 
-def reconstruct_shares(x, shares, prime):
+def reconstruct_shares(x, shares, prime = DEFAULT_PRIME):
     total = 0
 
     for i, (xj, yj) in enumerate(shares):
